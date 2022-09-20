ae75a081f3b9b7ffee40ce4b3b6286b6045dcf3d
Ivano0ovIvan <i_ivano0ov@abv.bg>
2022-09-20 21:20:07 +0300

    delete

--- basic syntax exercises/basic_sintax_exercise/double_char.py
+++
@@ -1,6 +0,0 @@
-command = input()
-while command != 'End':
-    if command == 'SoftUni':
-        continue
-    for letter in range(len(command)):
-        print(command[letter])
--- basic syntax exercises/basic_sintax_exercise/maximum_multiple.py
+++
@@ -1,6 +0,0 @@
-devisor = int(input())
-boundary = int(input())
-for current_number in range(boundary, devisor - 1, -1):
-    if current_number % devisor == 0:
-        break
-print(current_number)

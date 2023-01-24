987f8393106a160275ea5c2fff3755b6c3a80716
Ivan Nikolaev Ivanov <114014853+Ivano0ovIvan@users.noreply.github.com>
2023-01-24 17:06:36 +0200

    Add files via upload

---
+++ data_types_and_variables_lab/next_happy_year.py
@@ -0,0 +1,4 @@
+year = int(input())
+happy_year_condition = False
+while not happy_year_condition :
+    year += 1
---
+++ data_types_and_variables_lab/pounds_to_dollars.py
@@ -0,0 +1,3 @@
+pound = int(input())
+dollar = pound * 1.31
+print(f'{dollar:.3f}')

71d528930f90a7525ddad8f544fad01400312c24
Ivan Nikolaev Ivanov <114014853+Ivano0ovIvan@users.noreply.github.com>
2023-01-24 17:02:19 +0200

    Delete triples_of_latin_letters.py

--- triples_of_latin_letters.py
+++
@@ -1,5 +0,0 @@
-number_of_latters = int(input())
-for first_letters in range(number_of_latters):
-    for second_letter in range(number_of_latters):
-        for third_letter in range(number_of_latters):
-            print(f'{chr(97 + first_letters)}{chr(97 + second_letter)}{chr(97 + third_letter)}')

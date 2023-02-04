78a9ac7f31b89d0b6848ab42decca9a2df7e7c9d
Ivan Nikolaev Ivanov <114014853+Ivano0ovIvan@users.noreply.github.com>
2023-02-04 10:42:19 +0200

    Delete sort.py

--- sort.py
+++
@@ -1,8 +0,0 @@
-def sorted(current_numbers):
-    current_numbers.sort(reverse=False)
-    return current_numbers
-
-
-numbers = input()
-numbers_as_int = list(map(int, numbers.split()))
-print(sorted(numbers_as_int))

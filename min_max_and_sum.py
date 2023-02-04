8afb0392bb8be03e422d844524a536ad738ac55f
Ivan Nikolaev Ivanov <114014853+Ivano0ovIvan@users.noreply.github.com>
2023-02-04 10:43:24 +0200

    Delete min_max_and_sum.py

--- min_max_and_sum.py
+++
@@ -1,5 +0,0 @@
-numbers = input()
-numbers_as_int = list(map(int, numbers.split()))
-print(f"The minimum number is {min(numbers_as_int)}")
-print(f"The maximum number is {max(numbers_as_int)}")
-print(f"The sum number is: {sum(numbers_as_int)}")
